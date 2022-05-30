977812064945151ef2c89060924e0ef4e30eeb5b
Mehdi Allali <mehdi.allali1@etu.univ-lorraine.fr>
2022-05-30 16:39:42 +0200

    création de la methode configurerTouches et des attributs de touches

--- zeldiablo/src/moteurJeu/Clavier.java
+++ zeldiablo/src/moteurJeu/Clavier.java
@@ -1,8 +1,17 @@
 package moteurJeu;
 
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+
 public class Clavier {
+    /**
+    * touches du clavier
+     */
+    private KeyCode toucheHaut, toucheBas, toucheGauche, toucheDroite;
 
     /**
      * controle appuyes
@@ -72,4 +81,15 @@ public class Clavier {
 
         }
     }
+
+    /**
+     * configure les touches qui controlent le personnage
+     */
+    /*public void configurerTouches(){
+        try {
+            FileInputStream fichier = new FileInputStream("confi");
+            BufferedReader br = new BufferedReader();
+        }
+
+    }*/
 }
